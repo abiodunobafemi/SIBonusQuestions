1d2cc07e2daec79ce5c70ca1a1a07ac489622006
abiodunobafemi <59652204+abiodunobafemi@users.noreply.github.com>
2021-02-23 19:04:31 -0500

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,16 @@
+// Abiodun Obafemi
+
+public class SISession1 {
+
+	public static void main(String[] args) {
+		
+		// Output
+		System.out.print("I love SI "); 
+		System.out.print("sessions"); 
+		System.out.println("!!!!"); 
+		System.out.print("I love"); 
+		System.out.println(" to add!!!"); 
+		System.out.print("2 + 3 is "); 
+		System.out.print(2 + 3);
+	}
+}
